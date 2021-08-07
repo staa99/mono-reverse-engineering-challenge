a03e9b3c050e3772cb4138e107719f38a302f69a
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2021-08-07 05:03:14 +0100

    Implement GTBank get transactions

--- lib/gtbank/scraper/README.md
+++ lib/gtbank/scraper/README.md
@@ -1,1 +1,12 @@
-The scraper uses the requests library and cheerio
+The scraper uses the requests library and cheerio. Due to the tight time constraints, certain features are not
+implemented.
+
+- Keeping complexities to a minimum, account selection is disabled.
+
+- Helpful error messages are also not shown.
+
+- A constant device fingerprint is used.
+
+- No distributed cache is used for session management.
+
+- Session expiry detection is not implemented
--- routes/gtbank.js
+++ routes/gtbank.js
@@ -1,33 +1,31 @@
-const express = require('express');
-const { scraper } = require('../lib/gtbank/scraper');
-const router = express.Router();
+const express = require('express')
+const { scraper } = require('../lib/gtbank/scraper')
+const router = express.Router()
 
 router.get('/', function(req, res, next) {
   res.json({
     status: 'success',
     message: 'GTBank Internet Banking'
-  });
-});
+  })
+})
 
 router.post('/login', async function(req, res, next) {
-  const { username, password } = req.body;
+  const { username, password } = req.body
   const result = await scraper.login(username, password)
-  res.json(result);
-});
+  res.json(result)
+})
 
-router.get('/transactions', async function(req, res, next) {
-
-  res.json({
-    status: 'success',
-    message: 'GTBank Internet Banking'
-  });
-});
+router.get('/:sessionId/transactions', async function(req, res, next) {
+  const { sessionId } = req.params
+  const result = await scraper.getTransactions(sessionId)
+  res.json(result)
+})
 
 router.get('/transfer', async function(req, res, next) {
   res.json({
     status: 'success',
     message: 'GTBank Internet Banking'
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
